dcfa68c5d1cdf86d7c6e920b758d3b4e40410ec7
WeZZard <me@wezzard.com>
2024-02-12 19:01:15 +0800

    Initial Commit

---
+++ DependOnDefAndUseDemo/DependOnDefAndUseDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DependOnDefAndUseDemoApp.swift
+//  DependOnDefAndUseDemo
+//
+//  Created by WeZZard on 2/12/24.
+//
+
+import SwiftUI
+
+@main
+struct DependOnDefAndUseDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
